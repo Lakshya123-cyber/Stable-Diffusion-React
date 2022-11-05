f294c2efa3b4a991ec32d11449add0dfd0050c9b
Lakshya123-cyber <lakshya.raikwal@giis.edu.sg>
2022-11-05 22:37:21 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  ChakraProvider,
+  Heading,
+  Container,
+  Text,
+  Input,
+  Button,
+  Wrap,
+  Stack,
+  Image,
+  Link,
+  SkeletonCircle,
+  SkeletonText,
+} from "@chakra-ui/react";
 
-function App() {
+import axios from "axios";
+import { useState } from "react";
+
+const App = () => {
+  const [image, updateImage] = useState();
+  const [prompt, updatePrompt] = useState();
+  const [loading, updateLoading] = useState();
+
+  const generate = async (prompt) => {
+    updateLoading(true);
+    const result = await axios.get(`http://127.0.0.1:8000/?prompt=${prompt}`);
+    updateImage(result.data);
+    updateLoading(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider>
+      <Container>
+        <Heading>Stable Diffusion 🚀 </Heading>
+
+        <Text marginBottom={"10px"}>
+          This react application leverages the model trained by Stability AI and
+          Runway ML to generate images using the Stable Diffusion Deep Learning
+          model. The model can be found via github here{" "}
+          <Link href={"https://github.com/CompVis/stable-diffusion"}>
+            Github Repo
+          </Link>
+        </Text>
+
+        <Wrap marginBottom={"13px"}>
+          <Input
+            value={prompt}
+            onChange={(e) => updatePrompt(e.target.value)}
+            width={"350px"}
+          ></Input>
+          <Button onClick={(e) => generate(prompt)} colorScheme={"yellow"}>
+            Generate
+          </Button>
+        </Wrap>
+
+        {loading ? (
+          <Stack>
+            <SkeletonCircle />
+            <SkeletonText />
+          </Stack>
+        ) : image ? (
+          <Image src={`data:image/png;base64,${image}`} boxShadow="lg" />
+        ) : null}
+      </Container>
+    </ChakraProvider>
   );
-}
+};
 
 export default App;
